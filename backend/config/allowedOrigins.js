a42c56338d57b35bbd38926978e6adace063e66b
Sheldon Davis <sheldon.davis1515@gmail.com>
2023-07-26 23:37:39 -0400

    add ip for running across local network

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,5 +2,7 @@ const allowedOrigins = [
     'https://www.mySite.com',
     'http://localhost:5000',
     'http://localhost:3000',
+    'http://192.168.1.151:3000',
+    'http://192.168.1.151:5000'
 ]
 export default allowedOrigins
