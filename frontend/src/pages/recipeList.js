076f604ee738a0298b4f2b1125f058a1f4bcc8a0
Sheldon Davis <sheldon.davis1515@gmail.com>
2023-07-29 01:23:21 -0400

    meal planning components and function

--- frontend/src/pages/recipeList.js
+++ frontend/src/pages/recipeList.js
@@ -4,12 +4,14 @@ import {Link} from 'react-router-dom'
 //components
 import SkeletonRecipe from '../components/skeletons/SkeletonRecipe'
 import SingleRecipe from '../components/singleRecipe'
+import CreatingMealplan from '../components/creatingMealplan'
 
 //hooks
 import useAuth from '../hooks/useAuth'
 import { useAxiosPrivate } from '../hooks/useAxiosPrivate'
 import useInput from '../hooks/useInput'
 import useRecipes from '../hooks/useRecipes'
+import useArrayList from '../hooks/useArrayList'
 
 //begin createing the recipe list page
 const RecipeList = (props) => {
@@ -39,6 +41,9 @@ const RecipeList = (props) => {
     }
   },[])
 
+  //add hook for accessing and using mealplan hook
+  const {value, addOrRemove} = useArrayList('mealplan',[])
+  
   //intersection observer reference
   const intObserver = useRef()
   //when user scrolls close enough to the last recipe, load more...
@@ -67,6 +72,7 @@ const RecipeList = (props) => {
     resetKeywords('')
     setPageNum(0)
   }
+
 
   return(
     <>
@@ -104,13 +110,25 @@ const RecipeList = (props) => {
             {results.map((recipe, i, key)=>{
               if(results.length===i+1){
                 // console.log('last element')
-                return <SingleRecipe ref={lastRecipeRef} recipe={recipe} key={recipe._id}/>
+                return <SingleRecipe 
+                  ref={lastRecipeRef} 
+                  recipe={recipe}
+                  key={recipe._id} 
+                  addOrRemove={addOrRemove}
+                  mealplan={value}
+                />
               }
 
-              return <SingleRecipe recipe={recipe} key={recipe._id}/>
+              return <SingleRecipe 
+                recipe={recipe} 
+                key={recipe._id} 
+                addOrRemove={addOrRemove}
+                mealplan={value}
+              />
                 
             })}
             </section>
+            <CreatingMealplan mealplan={value}/>
             {isLoading && //show skeleton while loading
               [...Array(5).keys()].map((i, key)=>{
                 return <SkeletonRecipe key={key}/>
@@ -131,128 +149,9 @@ const RecipeList = (props) => {
         </>
       )
     }
+    
     </>
   )
-  // return (
-  //   <>
-  //     {auth.accessToken
-  //     ?(
-  //       <>
-  //       {
-  //         isLoading 
-  //         ? (
-  //           [...Array(20).keys()].map((i, key)=>{
-  //             return <SkeletonRecipe key={key}/>
-  //           })
-  //         ):(
-  //           <>
-  //             <section className='RecipeFilters'>
-  //               <form className='FilterForm'  onSubmit={handleFiltersSubmit}>
-  //                 <div className='recipeFiltersRow'>
-  //                   <div className='input-group'>
-  //                     <label htmlFor='RecipeTextInput'>Search for Keywords:</label>
-  //                     <input 
-  //                       type='text' 
-  //                       placeholder='sandwich, chicken, grilled etc.' 
-  //                       id='RecipeTextInput' 
-  //                       className='clearOnReset' 
-  //                       {...keywordsAttribs}
-  //                     />
-  //                     <button type='submit' 
-  //                     >Search</button>
-  //                   </div>
-  //                 </div>
-  //                 <div className='recipeFiltersRow'>
-  //                   <div className='input-group'>
-  //                     <label htmlFor='resultsPerPage'>
-  //                       Recipes per page: 
-  //                     </label>
-  //                     <select
-  //                       id='resultsPerPage'
-  //                       onChange={(e)=>{setRecipesPerPage(e.target.value)}}
-  //                       value={recipesPerPage}
-  //                     >
-  //                       <option value='-1'>All</option>
-  //                       <option value='20'>20</option>
-  //                       <option value='10'>10</option>
-  //                       <option value='5'>5</option>
-  //                     </select>
-  //                   </div>
-  //                 </div>
-                
-  //                 <div className='recipeFiltersRow'>
-  //                   <button type='button' onClick={()=>{handleClearForm()}}>Clear Filters</button>
-  //                 </div>
-  //               </form>
-  //             </section>
-  //             {recipes?.length
-  //               ?(
-  //                 <section className='allRecipes'>
-  //                   {
-  //                     recipes.map((recipe,i) =>{
-  //                       if(recipes.length===i+1){
-  //                         console.log(`last element = ${recipe.recipename}`)
-  //                         return <SingleRecipe recipe={recipe} key={recipe._id} ref={lastRecipeRef}/>
-  //                       }
-  //                       return <SingleRecipe recipe={recipe} key={recipe._id}/>
-                        
-  //                     }
-
-  //                     )
-  //                   }
-  //                   {
-  //                     pages>1 
-  //                     ?(
-  //                       <div className='Pager'>
-  //                         <button 
-  //                           type='button' 
-  //                           onClick={canPagePrev ? handleShowPreviousPage : (e)=>{}} 
-  //                           disabled={canPagePrev ? false : true}
-  //                         >
-  //                           &lt;&lt;
-  //                         </button>
-  //                         {
-  //                           [...Array(pages)].map((it, index) => {
-  //                             return  <button 
-  //                                       key={index} 
-  //                                       type='button' 
-  //                                       className={currentPage===index?'currentPage': ''} 
-  //                                       onClick={(e)=>handleShowSpecificPage(index)}
-  //                                     >
-  //                                       {index+1}
-  //                                     </button>
-  //                           })
-  //                         }
-  //                         <button 
-  //                           type='button' 
-  //                           onClick={handleShowNextPage} 
-  //                           disabled={canPageNext ? false : true}
-  //                         >
-  //                           &gt;&gt;
-  //                         </button>
-                          
-  //                       </div>
-  //                     )
-  //                     :(<></>)
-  //                   }
-  //                 </section>
-  //               ):(
-  //                 <p>No recipes to display.</p>
-  //               )
-
-  //             }  
-  //           </>
-  //         )}
-  //       </>
-  //     ):(
-  //       <>
-  //         <p>Login to view recipes</p>
-  //         <Link to='/login'>Log in</Link>
-  //       </>
-  //     )}
-      
-  //   </>
-  // );
 }
 
 export default RecipeList;
