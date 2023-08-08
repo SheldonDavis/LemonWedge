aa142fbc094bd6048bc3ff3b84166515fc84a338
Sheldon Davis <sheldon.davis1515@gmail.com>
2023-08-07 20:34:21 -0400

    add connection for pulling mealplan data

--- frontend/src/services/mealplan.serv.js
+++ frontend/src/services/mealplan.serv.js
@@ -5,6 +5,10 @@ const MealplanDataService = {
     //write data to DB
     saveMealplan:async function(data){
         return axiosPrivate.post('/mealplan/create',data)
+    },
+    //get mealplan and attach recipes
+    getMealPlan:async function(id){
+        return axiosPrivate.post('/mealplan/latest?user='+id)
     }
 
 }
