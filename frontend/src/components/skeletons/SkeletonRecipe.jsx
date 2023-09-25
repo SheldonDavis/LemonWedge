b4eb05fcdb1d79b6fb4bfc199ee26bf35f4505d5
Sheldon Davis <sheldon.davis1515@gmail.com>
2023-09-25 16:19:39 -0400

    update styles to match color scheme

--- frontend/src/components/skeletons/SkeletonRecipe.jsx
+++ frontend/src/components/skeletons/SkeletonRecipe.jsx
@@ -2,7 +2,7 @@ import Skeleton from './Skeleton.jsx'
 
 const SkeletonRecipe = () => {
     return(
-        <div className='singleRecipe'>
+        <div className='singleRecipe pad1_2rem'>
             <Skeleton classes={'title width-50'}/>
             <Skeleton classes={'title width-100'}/>
             <Skeleton classes={'title width-100'}/>
