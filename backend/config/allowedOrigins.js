b493d1ae8d4e248c2fd05506778cd59beee4901e
Sheldon Davis <sheldon.davis1515@gmail.com>
2023-09-26 09:40:53 -0400

    add sudo site to allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
-    'https://www.mySite.com',
     'http://localhost:5000',
     'http://localhost:3000',
     'http://192.168.1.151:3000',
-    'http://192.168.1.151:5000'
+    'http://192.168.1.151:5000',
+    'https://lemonwedge.onrender.com'
 ]
 export default allowedOrigins
