4f145b10e37ebce69c028659fea6eea30a8ceb43
Sheldon Davis <sheldon.davis1515@gmail.com>
2023-10-02 21:10:23 -0400

    remove ip from allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5000',
     'http://localhost:3000',
-    'http://192.168.1.151:3000',
-    'http://192.168.1.151:5000',
     'https://lemonwedge.onrender.com'
 ]
 export default allowedOrigins
