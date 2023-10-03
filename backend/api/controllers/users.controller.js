5e332d4c07933566be29a12c9dbe7fcb4631ecea
Sheldon Davis <sheldon.davis1515@gmail.com>
2023-10-03 10:16:17 -0400

    remove domain declaration from cookie creation

--- backend/api/controllers/users.controller.js
+++ backend/api/controllers/users.controller.js
@@ -226,8 +226,7 @@ export default class UserController{
                     maxAge:24*60*60*1000,
                     httpOnly:true, 
                     secure:true,
-                    sameSite:'lax', 
-                    domain:'https://lemonwedgetesting.onrender.com'
+                    sameSite:'lax',
 
                 }
                 res.cookie('jwt', refreshToken, cookieOptions)
