e9d33ee42a68fa355656d126601824320f8f8c2f
Sheldon Davis <sheldon.davis1515@gmail.com>
2023-10-05 12:16:34 -0400

    update link to base URL for backend

--- frontend/src/api/http-common.js
+++ frontend/src/api/http-common.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 //url where abckend API lives
-// const BASE_URL = 'http://localhost:3000/api/v1'
-// const BASE_URL = 'https://lemonwedgebackend.onrender.com/api/v1'
-const BASE_URL = 'https://lemonwedge.onrender.com/api/v1'
+// const BASE_URL = 'http://localhost:3000/api/v1'//testing locally
+const BASE_URL = 'https://lemonwedge.onrender.com/api/v1'//live site
 
 
 //base level api access
