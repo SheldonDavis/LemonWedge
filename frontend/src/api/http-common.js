cc269ce233f8787a9e2627319addfde53ac6af61
Sheldon Davis <sheldon.davis1515@gmail.com>
2023-11-06 08:58:17 -0500

    dynamic API path reference

--- frontend/src/api/http-common.js
+++ frontend/src/api/http-common.js
@@ -2,9 +2,15 @@ import axios from 'axios'
 
 //url to API
 const location = window.location.origin.toString()
-const BASE_URL = location+'/api/v1'
+let passingURL = ''
+if(location.includes('localhost')){    //For testing. making sure API path is referencing the location of seperate port
+    passingURL = 'http://localhost:5000/api/v1'
+}else{
+    passingURL = location+'/api/v1'
+}
+const BASE_URL = passingURL
 
-//base level api access
+//base level API access
 export default axios.create({
     baseURL:BASE_URL,
     headers:{
